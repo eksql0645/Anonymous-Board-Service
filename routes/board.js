20bd590ab040c36596157eb17620066f1f2300da
JoKyungseo <eksql0645@gmail.com>
2022-09-06 20:27:55 +0900

    [Feat] 게시글 수정 API 구현

--- routes/board.js
+++ routes/board.js
@@ -4,9 +4,22 @@ const { boardValidator } = require("../middlewares/validator/boardValidator");
 const {
   passwordValidator,
 } = require("../middlewares/validator/passwordValidator");
+const { nameValidator } = require("../middlewares/validator/nameValidator");
 const router = Router();
 
-router.post("/", boardValidator(), boardService.addPost);
+router.post(
+  "/",
+  boardValidator(),
+  nameValidator(),
+  passwordValidator(),
+  boardService.addPost
+);
 router.get("/:id", boardService.getPost);
+router.patch(
+  "/:id",
+  boardValidator(),
+  passwordValidator(),
+  boardService.setPost
+);
 router.delete("/:id", passwordValidator(), boardService.deletePost);
 module.exports = router;
