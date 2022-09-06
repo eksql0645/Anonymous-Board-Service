2b2407e56897cf8df8e12a9f25a817a8b91b4a2b
JoKyungseo <eksql0645@gmail.com>
2022-09-06 12:17:03 +0900

    [Feat] Sequelize DB 연결 설정

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { sequelize } = require("./db");
 const dotenv = require("dotenv");
-const { swaggerUi, specs } = require("./swagger/swagger");
+// const { swaggerUi, specs } = require("./swagger/swagger");
 
 dotenv.config();
 
 const app = express();
+app.set("port", process.env.PORT);
 
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Synced database.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync database: " + err.message);
+  });
+
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan("dev"));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 module.exports = app;
