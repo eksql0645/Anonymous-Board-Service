db8e58a8fadab7f50ce4305452be14b7a188c91c
JoKyungseo <eksql0645@gmail.com>
2022-09-07 15:44:13 +0900

    [Refactor] weather try-catch 추가

--- middlewares/weather.js
+++ middlewares/weather.js
@@ -2,15 +2,19 @@ require("dotenv").config();
 const requestPromise = require("request-promise");
 
 async function weather() {
-  const options = {
-    url: `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=seoul&lang=ko`,
-    method: "GET",
-    headers: { "content-type": "application/json" },
-  };
+  try {
+    const options = {
+      url: `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=seoul&lang=ko`,
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    };
 
-  let res = await requestPromise(options);
-  res = JSON.parse(res);
-  return res.current.condition.text;
+    let res = await requestPromise(options);
+    res = JSON.parse(res);
+    return res.current.condition.text;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = weather;
